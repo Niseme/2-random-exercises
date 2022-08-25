0ba517deefb187e9fbe1f9975bd2b15c7bb6bcf7
Niseme <nijolesemele@gmail.com>
2022-08-25 11:40:22 +0300

    add exercises 1,3,5,6

--- index.js
+++ index.js
@@ -23,3 +23,43 @@ function lastElements(arr, n) {
 
 console.log(lastElements([1, 2, 3, 4, 5], 3));
 
+//Rest of Homework
+
+//1.Write a function that takes two numbers (a and b) as Sum a and Return the result
+
+function sum(a, b) {
+    const suma = a + b;
+    return suma;
+}
+
+console.log(sum(2, 5))
+
+//3. Write a function that takes a number(a) as argument Split a into its individual digits and return them in an array.
+
+function splitNum(a) {
+    const splitedNum = a.toString().split('');
+    return splitedNum;
+}
+
+console.log(splitNum(1989))
+
+//5. Write a function that takes two arrays as arguments Merge both arrays and remove duplicate values Sort the merge result in ascending order Return the resulting array
+function mergeingArr(arr1, arr2) {
+
+    const set = new Set(arr1.concat(arr2));
+    const [...setArr] = set;
+    const sortArr = setArr.sort()
+
+    console.log(sortArr)
+
+}
+mergeingArr([1, 3], [2, 1])
+
+//6. Write a function that takes an object(a) and a string(b) as argument  Return true if the object has a property with key 'b' Return false otherwise
+function objStr(a, b) {
+    return a.hasOwnProperty(b)
+}
+console.log(objStr({ name: "Nijole" }, 'name'))
+
+
+
